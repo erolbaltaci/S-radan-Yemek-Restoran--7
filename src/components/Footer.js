a3add717fcee77a012e1a196153a0241b4023cf7
Anaokulu Uygulaması <baltaci.tml@gmail.com>
2024-02-12 13:35:13 +0300

    start setup

---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+const Footer = () => {
+    return (
+        <footer>
+          <p>
+            Made by <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" target="_blank" rel="noopener noreferrer"> Coding Company</a>™.
+          </p>
+        </footer>
+      );
+}
+export default Footer
---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+const Header = () => {
+  return (
+    <header>
+      <div class="button-container">
+        <button>Menu</button>
+        <button>Hakkında</button>
+        <button>Saatler & Lokasyon</button>
+      </div>
+    </header>
+  );
+};
+export default Header;
